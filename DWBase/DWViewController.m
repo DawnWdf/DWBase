7527adb6067c283e28b647ce4542cbd198fc6b9f
Dawn <icy19882006@163.com>
2017-03-14 14:59:45 +0800

    config

--- DWBase/DWViewController.m
+++ DWBase/DWViewController.m
@@ -19,6 +19,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self configNav];
+    [self configView];
+    [self layoutView];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -44,14 +47,19 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - config view
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)configNav{
+
 }
-*/
+
+- (void)configView {
+    
+}
+
+
+- (void)layoutView {
+    
+}
 
 @end
